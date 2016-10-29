0dbe9511006978ba7878ee79c24fa243c1302477
Iván Rodriguez <ivan.rodriguez@bq.com>
2016-10-29 19:00:59 +0200

    Add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+const DIST_PATH = path.resolve(__dirname, 'dist');
+const SOURCE_PATH = path.resolve(__dirname, 'src');
+
+module.exports = {
+    entry: `${SOURCE_PATH}/app.js`,
+    module: {
+        loaders: [{
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+            query: {
+                presets: [
+                    'es2015',
+                    'react',
+                    'stage-2',
+                ],
+            },
+            test: /.jsx?$/,
+        }],
+    },
+    output: {
+        filename: 'app.dist.js',
+        path: DIST_PATH,
+        publicPath: '/assets/',
+    },
+};
