19262a92e1d04c2cbe13563b3a10c768290a45ac
Iván Rodriguez <ivan.rodriguez@bq.com>
2016-11-13 20:43:40 +0100

    Add repositories prop to GithubList component

--- src/components/GithubList/GithubList.jsx
+++ src/components/GithubList/GithubList.jsx
@@ -3,7 +3,7 @@ import radium from 'radium';
 import { Grid, Cell } from 'radium-grid';
 import styles from './githubListStyles';
 
-const GithubList = () =>
+const GithubList = props =>
   <div
     style={[
       styles.base,
@@ -13,9 +13,14 @@ const GithubList = () =>
       <Cell align="center" width="1/2">
         <p>Github Repositories</p>
       </Cell>
+      <Cell>
+        <ul>
+          {props.repositories.map((repository, index) => <li key={index}>{repository}</li>)}
+        </ul>
+      </Cell>
     </Grid>
   </div>;
 
-GithubList.propTypes = {};
+GithubList.propTypes = { repositories: React.PropTypes.arrayOf(React.PropTypes.string) };
 
 export default radium(GithubList);
