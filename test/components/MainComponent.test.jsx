4bf91bbeab77dadb1d1256dd5697c71e44844e77
Iván Rodriguez <ivan.rodriguez@bq.com>
2016-12-12 18:09:05 +0100

    Add jest to run tests

---
+++ test/components/MainComponent.test.jsx
@@ -0,0 +1,8 @@
+import MainComponent from '../../src/components/MainComponent';
+import React from 'react';
+
+describe('Main Component', () => {
+  it('should be great', () => {
+    expect(true).toEqual(true);
+  });
+});
