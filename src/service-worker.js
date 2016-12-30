9f3f553968ce77ae38a33ec2f554ef7d5615328b
Iván Rodriguez <ivan.rodriguez@bq.com>
2016-12-30 20:23:03 +0100

    Add service worker versioning

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,7 +1,9 @@
+const staticCacheVersion = 'webcv-static-0.0.1';
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open('webcv-static-v1').then(cache =>
+    // Cache static files
+    caches.open(staticCacheVersion).then(cache =>
       cache.addAll([
         '/',
         '/app.dist.js',
@@ -10,7 +12,27 @@ self.addEventListener('install', (event) => {
   );
 });
 
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    // Delete older cache versions
+    caches.keys().then((cacheNames) => {
+      Promise.all(
+        cacheNames
+        .filter(cacheName =>
+          // Filter older webcv cache versions
+          cacheName.startsWith('webcv-') &&
+                 cacheName !== staticCacheVersion
+        )
+        .map(cacheName =>
+          caches.delete(cacheName)
+        )
+      );
+    })
+  );
+});
+
 self.addEventListener('fetch', (event) => {
+  // Respond static files with cache versions if possible
   event.respondWith(
     caches.match(event.request).then(response => response || fetch(event.request))
   );
